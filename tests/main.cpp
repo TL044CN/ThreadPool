cf1ac8a54559b50a4a58caa62666a90d2de7e0f0
TL044CN <gunnisan2@web.de>
2024-01-21 17:41:34 +0100

    CHG: Improved tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -18,13 +18,37 @@ int main() {
 
         } catch ( std::exception e ) {
             std::cout << "Exception: " << e.what() << "\n";
+            return EXIT_FAILURE;
         }
-        
+
         for ( auto& future : futures )
             std::cout << "i: " << future.get() << '\n';
 
+        try {
+            std::condition_variable cv;
+            std::mutex mutex;
+
+            mutex.lock();
+            auto future = tp.queueJob([&] {
+                std::unique_lock<std::mutex> lock(mutex);
+                cv.wait(lock, [] {return true;});
+                return 256;
+            });
+
+            mutex.unlock();
+            cv.notify_one();
+
+            if ( future.get() != 256 )
+                return EXIT_FAILURE;
+
+        } catch ( std::exception e ) {
+            std::cout << "Exception: " << e.what() << "\n";
+            return EXIT_FAILURE;
+        }
+
     } catch ( std::exception e ) {
         std::cout << "Exception: " << e.what() << "\n";
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
